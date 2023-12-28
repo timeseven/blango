4f2eb3c25b1ea474fddd0d27af49c017425b4e51
Even Qian <evenwin88@hotmail.com>
2023-12-28 02:55:16 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
 import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie 
 
 logger = logging.getLogger(__name__)
 
