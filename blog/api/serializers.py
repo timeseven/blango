bd90d992900fb453842b0328032e6637695a938e
Even Qian <evenwin88@hotmail.com>
2023-12-29 13:44:31 +0000

    Finish viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -55,3 +55,8 @@ class PostDetailSerializer(PostSerializer):
             comment.save()
 
         return instance
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
