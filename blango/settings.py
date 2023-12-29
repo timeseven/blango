818182121e7b03afbc6de95011bf2e87d1235800
Even Qian <evenwin88@hotmail.com>
2023-12-29 06:10:54 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -51,6 +51,7 @@ class Dev(Configuration):
         "django.contrib.sites",
         'django.contrib.staticfiles',
         'rest_framework',
+        'rest_framework.authtoken',
         "debug_toolbar",
         'crispy_forms',
         'crispy_bootstrap5',
@@ -221,6 +222,14 @@ class Dev(Configuration):
     ACCOUNT_EMAIL_REQUIRED = True
     ACCOUNT_USERNAME_REQUIRED = False
     ACCOUNT_AUTHENTICATION_METHOD = "email"
+
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
     
 class Prod(Dev):
     DEBUG = False
