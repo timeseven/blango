66a80d012b03b840b43274f4473bf83066b23615
Even Qian <evenwin88@hotmail.com>
2023-12-31 03:46:07 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -52,6 +52,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'rest_framework',
         'rest_framework.authtoken',
+        'django_filters',
         "debug_toolbar",
         'crispy_forms',
         'crispy_bootstrap5',
@@ -245,6 +246,12 @@ class Dev(Configuration):
             "user_sustained": "5000/day",
             "user_burst": "100/minute",
         },
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+            "rest_framework.filters.OrderingFilter"
+        ],
     }
 
     SWAGGER_SETTINGS = {
