edfae8e889ba43af1022428c441e462dddc4df4f
Even Qian <evenwin88@hotmail.com>
2024-01-01 00:01:23 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path("api/v1/", include("blog.api.urls")),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
 ]
 
 if settings.DEBUG:
